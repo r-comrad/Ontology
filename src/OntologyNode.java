d4f2cebe34ca5400d93c07029d7e75b5f29b92b5
Comrad <comrade_1997@mail.ru>
2019-08-04 21:45:34 +0300

    launching

--- src/OntologyNode.java
+++ src/OntologyNode.java
@@ -1,18 +1,24 @@
 import com.mxgraph.view.mxGraph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class OntologyNode {
     private String mСonceptName;
-    private Map<String, OntologyNode> mConnections = new HashMap<String, OntologyNode>();
-    //List<OntologyNode>
+    private Map<String, OntologyNode> mConnections;
     boolean isDrawed = false;
 
     public void addConnection(ArrayList<String> aPath, OntologyNode aNode) {
         OntologyNode node = getNode(aPath);
         node.mConnections.put(aNode.getCommandName(), aNode);
+    }
+
+    public void addConnection(OntologyNode aNode) {
+        mConnections.put(aNode.getCommandName(), aNode);
+    }
+
+    public OntologyNode(String aСonceptName) {
+        mСonceptName = aСonceptName;
+        mConnections = new HashMap<String, OntologyNode>();
     }
 
     public OntologyNode getNode(ArrayList<String> aPath) {
@@ -25,13 +31,36 @@ public class OntologyNode {
         }
     }
 
-    public void draw(mxGraph mGraph, Object aParent, Object aStartVertex)
-    {
-        if (!isDrawed)
-        {
+    public OntologyNode findNode(String aСonceptName) {
+        Queue<OntologyNode> queue = new LinkedList<>();
+        queue.offer(this);
+
+        OntologyNode result = null;
+        while (!queue.isEmpty()) {
+            OntologyNode node = queue.remove();
+            if (Objects.equals(node.mСonceptName, aСonceptName)) {
+                result = node;
+                break;
+            }
+            for (Map.Entry<String, OntologyNode> entry : node.mConnections.entrySet()) {
+                queue.offer(entry.getValue());
+            }
+        }
+        return result;
+    }
+
+    public void draw(mxGraph mGraph, Object aParent, Object aStartVertex) {
+        if (!isDrawed) {
             isDrawed = true;
-            Object vertex = mGraph.insertVertex(aParent, null, mСonceptName, 20, 20, 40, 20);
-            mGraph.insertEdge(aParent, null, "-", aStartVertex, vertex);
+            Object vertex = mGraph.insertVertex(aParent, null, mСonceptName,
+                    Math.random() * 200, Math.random() * 200, 40, 20);
+            if (mСonceptName != "start") {
+                mGraph.insertEdge(aParent, null, "-", aStartVertex, vertex);
+            }
+
+            for (Map.Entry<String, OntologyNode> entry : mConnections.entrySet()) {
+                entry.getValue().draw(mGraph, aParent, vertex);
+            }
         }
     }
 
--- src/ProgramPanel.java
+++ src/ProgramPanel.java
@@ -7,48 +7,33 @@ import com.mxgraph.view.mxGraph;
 
 public class ProgramPanel extends JPanel {
     public ProgramPanel() {
-        mxGraph graph = new mxGraph();
-        Object parent = graph.getDefaultParent();
-
-        Object v1;
-        graph.getModel().beginUpdate();
-        try
-        {
-            v1 = graph.insertVertex(parent, null, "Habrahhhhhhhhhhhhhhhhhhhhhh", 20, 20, 80, 30);
-            Object v2 = graph.insertVertex(parent, null, "Habr", 240, 150, 80, 30);
-            graph.insertEdge(parent, null, "Дуга", v1, v2);
-        }
-        finally
-        {
-            graph.getModel().endUpdate();
-        }
-        Object v3 = graph.insertVertex(parent, null, "Ha", 240, 20, 80, 30);
-        graph.insertEdge(parent, null, "Дуга", v1, v3);
-
-        mxGraphComponent graphComponent = new mxGraphComponent(graph);
-        add(graphComponent);
-        
-
-        String s = new String("hjhh");
-        String ss = s;
-        ss += "11";
-        System.out.println(s);
-        System.out.println(ss);
-
-
-        CommandTreeNode tree = new CommandTreeNode();
-        CommandTreeNode tree2 = tree;
+        OntologyNode ontology = new OntologyNode("start");
 
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
-                        new FileInputStream("D:/projects/Java/Ontology/res/" + "commands list.txt"), StandardCharsets.UTF_8))){
-            String command;
+                        new FileInputStream("D:/projects/Java/Ontology/res/" + "onyology.txt"), StandardCharsets.UTF_8))) {
+            /*String command;
             while ((command = reader.readLine()) != null) {
                 System.out.println(command);
                 tree.add(command.toCharArray(), 0);
+
+            }*/
+
+            for (int i = 0; i < 5; ++i) {
+                String vertix1 = reader.readLine(), vertix2 = reader.readLine();
+                OntologyNode node = ontology.findNode(vertix1);
+                OntologyNode newNode = new OntologyNode(vertix2);
+                node.addConnection(newNode);
             }
+
         } catch (IOException e) {
             // log error
         }
+
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+        ontology.draw(graph, parent, ontology);
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        add(graphComponent);
     }
 }
