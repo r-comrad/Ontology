46ac56eabd6b66029a9e3a46d50335a54640c13d
Comrad <comrade_1997@mail.ru>
2019-08-08 00:37:49 +0300

    file ontology reader

--- src/ProgramPanel.java
+++ src/ProgramPanel.java
@@ -1,13 +1,74 @@
 import javax.swing.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 
 public class ProgramPanel extends JPanel {
+    private OntologyNode mOntology;
+
+    public String fileReader(BufferedReader aReader)
+    {
+        String result = "";
+        try
+        {
+            int c;
+            while((c=aReader.read())!=-1 && ! Character.isWhitespace((char)c)){ // TODO: \f \r
+                //for(Character obj : aBreakChars)
+                result += (char)c;
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+
+    public void fileParser()
+    {
+        try(FileReader reader = new FileReader("aaa.txt"))
+        {
+            int c;
+            while((c=reader.read())!=-1){
+
+                System.out.print((char)c);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void ontologyReader()
+    {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream("D:/projects/Java/Ontology/res/" + "onyology.txt"), StandardCharsets.UTF_8))) {
+            int count = Integer.parseInt(fileReader(reader));
+            for(int i = 0; i < count; ++i)
+            {
+                String concept, parent;
+                while ((parent = fileReader(reader)) == "");
+                while ((concept = fileReader(reader)) == "");
+
+                OntologyNode node = mOntology.findNode(parent);
+                OntologyNode newNode = new OntologyNode(concept);
+                node.addConnection(newNode);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
     public ProgramPanel() {
-        OntologyNode ontology = new OntologyNode("start");
+        mOntology = new OntologyNode("start");
 
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
@@ -19,20 +80,22 @@ public class ProgramPanel extends JPanel {
 
             }*/
 
-            for (int i = 0; i < 13; ++i) {
+            /*for (int i = 0; i < 13; ++i) {
                 String vertix1 = reader.readLine(), vertix2 = reader.readLine();
                 OntologyNode node = ontology.findNode(vertix1);
                 OntologyNode newNode = new OntologyNode(vertix2);
                 node.addConnection(newNode);
-            }
+            }*/
 
         } catch (IOException e) {
             // log error
         }
 
+        ontologyReader();
+
         mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
-        ontology.drawCenter(graph, parent, ontology, 100, 100);
+        mOntology.drawCenter(graph, parent, mOntology, 100, 100);
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
         add(graphComponent);
     }
